88ce6fa73b5e1f2b64b53dbf059856aed53cef36
ThomasBement <46405515+ThomasBement@users.noreply.github.com>
2020-01-19 21:11:55 -0800

    fix formatting,

--- binary-roots.cpp
+++ binary-roots.cpp
@@ -1,58 +1,71 @@
-//-------------------------------------------------------//
-//  Problem Statement:
+//---------------------------------------------------------------//
+//  Problem Statement: Find first positive root of Sine function
+//                     to six digits.
 //  Name: Thomas Bement
-//  SN: 24099822
+//  SN: XXXXXXX
 //  UBC Oakanagan
 //  Copyright 2018, All rights reserved
-//-------------------------------------------------------//
+//---------------------------------------------------------------//
+
 #include <iostream>
 #include <string>
 #include <math.h>
 
 using namespace std;
+
 long double ldubFunc(long double x){
     return sin(x);
 }
 
-int main(){
-bool bAnswer=false;
-long double ldubLower=4;
-long double ldubUpper=2;
-long double ldubTest = (ldubLower+ldubUpper)/2;
-cout<<"Test Points: \n"<<ldubTest<<endl;
-while (bAnswer==false) {
-    if (ldubFunc(ldubTest) == 0) {
-        cout<<"Zero is at: "<<ldubTest<<endl;
-        bAnswer=true;
+int main()
+{
+    bool bAnswer=false;
+    long double ldubEpsilon = 0.000008;
+    long double ldubLower = 4.0;
+    long double ldubUpper = 2.0;
+    long double ldubTest = (ldubLower+ldubUpper)/2.0;
+
+    cout<<"Test Points: \n"<<ldubTest<<endl;
+
+    while (bAnswer==false) {
+        if (fabs(ldubFunc(ldubTest)) <= ldubEpsilon) {
+            cout<<"Zero is near: "<<ldubTest<<endl;
+            bAnswer=true;
+        }
+        else if (ldubFunc(ldubTest) > 0) {
+            if (ldubFunc(ldubUpper) > 0) {
+                // f(test) is + and f(upper) is also + so theres no zero
+                // in that range and upper is updated to test
+                //
+                ldubUpper = ldubTest;
+                ldubTest = (ldubLower+ldubUpper)/2.0;
+                cout<<ldubTest<<endl;
+            }
+            else {
+                // f(test) is + and f(upper) is - so there is a zero in
+                // that range so lower is updated to test
+                //
+                ldubLower = ldubTest;
+                ldubTest = (ldubLower+ldubUpper)/2.0;
+                cout<<ldubTest<<endl;
+            }
+        }
+        else if (ldubFunc(ldubTest) < 0) {
+            if (ldubFunc(ldubUpper) < 0) {
+                // f(test) is - and f(upper) is also - so theres no zero
+                // in that range and upper is updated to test
+                ldubUpper = ldubTest;
+                ldubTest = (ldubLower+ldubUpper)/2.0;
+                cout<<ldubTest<<endl;
+            }
+            else {
+                // f(test) is - and f(upper) is + so there is a zero in
+                // that range so lower is updated to test
+                ldubLower = ldubTest;
+                ldubTest = (ldubLower+ldubUpper)/2.0;
+                cout<<ldubTest<<endl;
+            }
+        }
     }
-    else if (ldubFunc(ldubTest) > 0) {
-        if (ldubFunc(ldubUpper) > 0) {
-            // f(test) is + and f(upper) is also + so theres no zero in that range and upper is updated to test
-            ldubUpper = ldubTest;
-            ldubTest = (ldubLower+ldubUpper)/2;
-            cout<<ldubTest<<endl;
-        }
-        else {
-            //f(test) is + and f(upper) is - so there is a zero in that range so lower is updated to test
-            ldubLower = ldubTest;
-            ldubTest = (ldubLower+ldubUpper)/2;
-            cout<<ldubTest<<endl;
-        }
-    }
-    else if (ldubFunc(ldubTest) < 0) {
-        if (ldubFunc(ldubUpper) < 0) {
-            // f(test) is - and f(upper) is also - so theres no zero in that range and upper is updated to test
-            ldubUpper = ldubTest;
-            ldubTest = (ldubLower+ldubUpper)/2;
-            cout<<ldubTest<<endl;
-        }
-        else {
-            //f(test) is - and f(upper) is + so there is a zero in that range so lower is updated to test
-            ldubLower = ldubTest;
-            ldubTest = (ldubLower+ldubUpper)/2;
-            cout<<ldubTest<<endl;
-        }
-    }
-}
     return 0;
 }
